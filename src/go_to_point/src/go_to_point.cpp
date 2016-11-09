5328e9a248178d12477d140e2c930061000ef5c0
Soren MV <svoss16@student.aau.dk>
2016-11-09 10:59:31 +0100

    init2

--- src/go_to_point/src/go_to_point.cpp
+++ src/go_to_point/src/go_to_point.cpp
@@ -16,13 +16,6 @@ private:
 	ros::Publisher marker_pub;
 	ros::Subscriber click_sub;
 
-	union geometryPoint {
-		float64 x;
-		float64 y;
-		float64 z;
-
-	};
-
 	void _send_goal(const geometry_msgs::PointStamped& goal_point)
 	{
 		move_base_msgs::MoveBaseGoal goal;
@@ -32,7 +25,7 @@ private:
 		_send_markers();
 	}
 
-	void _go_to_point(const geometryPoint& goal_point)
+	void _go_to_point(const geometry_msgs::PointStamped& goal_point)
 	{
 		move_base_msgs::MoveBaseGoal goal;
 		goal.target_pose.header.frame_id = goal_point.header.frame_id;
@@ -42,73 +35,38 @@ private:
 
 	void _target_reached_cb(const actionlib::SimpleClientGoalState& state, const move_base_msgs::MoveBaseResultConstPtr& result)
 	{
-		std::swap(from, to);
-		_send_goal(to);
+		// std::swap(from, to);
+		// _send_goal(to);
 	}
 
 	void _send_markers()
 	{
-		visualization_msgs::Marker marker;
-		marker.header.stamp = ros::Time::now();
-		marker.ns = "bus_stops";
-		marker.type = visualization_msgs::Marker::ARROW;
-		marker.action = visualization_msgs::Marker::ADD;
-		marker.scale.x = 1.0;
-		marker.scale.y = 0.2;
-		marker.scale.z = 0.2;
-		marker.color.r = 0.0;
-		marker.color.g = 1.0;
-		marker.color.b = 0.0;
-		marker.color.a = 1.0;
-		marker.pose.orientation.x = 0;
-		marker.pose.orientation.y = 0.7071;
-		marker.pose.orientation.z = 0;
-		marker.pose.orientation.w = 0.7071;
-		marker.lifetime = ros::Duration();
-
-		visualization_msgs::MarkerArray marker_array;
-		if (stops_initialized > 0)
-		{
-			marker.header.frame_id = to.header.frame_id;
-			marker.id = 0;
-			marker.pose.position = to.point;
-			marker.pose.position.z += marker.scale.x;
-			marker_array.markers.push_back(marker);
-		}
-		if (stops_initialized > 1)
-		{
-			marker.header.frame_id = from.header.frame_id;
-			marker.id = 1;
-			marker.pose.position = from.point;
-			marker.pose.orientation.w = -0.7071;
-			marker_array.markers.push_back(marker);
-		}
-		marker_pub.publish(marker_array);
+		
 	}
 
-	void _clicked_point_cb(const geometry_msgs::PointStamped::ConstPtr& msg)
+	
+	void _command_send_cb(const String& msg)
 	{
-		ROS_INFO("Clicked: %f, %f, %f", msg->point.x,
-			msg->point.y, msg->point.z);
-		if (stops_initialized > 0)
-		{
-			from = to;
-		}
-		to = *msg;
+		ROS_INFO("Recived: \"%s\" command.", msg);
 
-		stops_initialized += 1;
+		const geometry_msgs::PointStamped& to;
+		
+		// Lookup db to associate a command string with coordinates
+		
+		// Hard coded coordinates:
+		to = {1,0,0,1};
 
-		_send_goal(to);
+
+		_go_to_point(to);
 	}
 
 public:
 	Route() :
-		client("move_base"),
-		stops_initialized(0)
+		client("move_base")
 	{
 		ros::NodeHandle n;
 		marker_pub = n.advertise<visualization_msgs::MarkerArray>("visualization_marker_array", 1);
-		click_sub = n.subscribe("clicked_point", 100, &Route::_clicked_point_cb, this);
+		click_sub = n.subscribe("command_send", 1, &Route::_command_send_cb, this);
 	};
 	~Route(){};
 

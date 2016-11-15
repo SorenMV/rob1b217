5117025809901433204382d4ce04de5c59b05f31
Soren MV <svoss16@student.aau.dk>
2016-11-15 15:13:13 +0100

    updated names

--- src/go_to_point/src/go_to_point.cpp
+++ src/go_to_point/src/go_to_point.cpp
@@ -5,46 +5,47 @@
 #include <sstream>
 
 
-class Route
+class GoToPoint
 {
 private:
+	// Initialize variables
 	move_base_msgs::MoveBaseGoal goal;
 	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> client;
-	ros::Publisher marker_pub;
-	ros::Subscriber click_sub;
 
-	// This function gets called when a coordinate is set and wants to move.
-	void _go_to_point(const move_base_msgs::MoveBaseGoal& goal_point)
+
+	// This function gets called when a coordinate is set and wants to move
+	void _go_to_point(const move_base_msgs::MoveBaseGoal& goal)
 	{
 		//wait for the action server to come up
-		while(!client.waitForServer(ros::Duration(5.0)))
+		while( ! client.waitForServer(ros::Duration(5.0)) )
 		{
 		    ROS_INFO("Waiting for the move_base action server to come up.");
 		}
 
-		// Send the goal to actionlib
-		client.sendGoal(goal_point, boost::bind(&Route::_target_reached_cb, this, _1));
+		// Send the goal to actionlib and return to the "callback" function
+		client.sendGoal(goal, boost::bind(&GoToPoint::_target_reached, this, _1));
 		ROS_INFO("Navigating ...");
 	}
 
-	// This function gets called when actionlib is done navigating to the goal.
-	void _target_reached_cb(const actionlib::SimpleClientGoalState& state)
+	// This function gets called when actionlib is done navigating to the goal
+	void _target_reached(const actionlib::SimpleClientGoalState& state)
 	{
+		// Check if actionlib successfully navigated to goal
 		if (state == actionlib::SimpleClientGoalState::SUCCEEDED)
 		{
 			ROS_INFO("Target reached.");
 		}
 		else
 		{
-			ROS_INFO("Failed to reach target.");
 			// This can be because of obsticles making it impossible to get to goal.
+			ROS_INFO("Failed to reach target.");
 		}
 	}
 
 	// This function gets called when a topic we subscribe to is recieved.
 	// A command with type "String" gets send and it will look-up to see
 	// if that string is saved. If it is, set the coordinates and pass them to next function.
-	void _command_send_cb(const std_msgs::String& command)
+	void _command_send(const std_msgs::String& command)
 	{
 		ROS_INFO("Recived command: %s", command.data.c_str());
 		
@@ -53,14 +54,16 @@ private:
 		// coordinate frame ("map", "base_link")
 		goal.target_pose.header.frame_id = "map";
 
+
 		// Hard coded coordinates<double>:
-		goal.target_pose.pose.position.x = 4.0;
-		goal.target_pose.pose.position.y = -3.0;
+		goal.target_pose.pose.position.x = 2.0;
+		goal.target_pose.pose.position.y = 1.0;
 		goal.target_pose.pose.orientation.z = 0.5;
 		goal.target_pose.pose.orientation.w = 0.5;
 
-		// home = [0.654, -1.07]
-		// hallway = [4.0, -3.0]
+		// home = [0.0, 0.0]
+		// hallway1 = [4.0, 1.0] ?
+		// hallway2 = [-3.0, 1.0] ?
 
 		// Send coordinates to next function
 		_go_to_point(goal);
@@ -68,41 +71,41 @@ private:
 
 public:
 	// Constructor
-	Route():
+	GoToPoint():
 		client("move_base", true)
 	{
 		// Standard ros NodeHandle
-		ros::NodeHandle n;
+		// ros::NodeHandle n;
 
 		// Make a publish element with type <std_msgs::String>
 		// marker_pub = n.advertise<std_msgs::String>("command_send", 1);
 
-		// subscribe("topic", queueSize, callbackFunction, hint)
-		// click_sub = n.subscribe("command_send", 10, &Route::_command_send_cb, this);
+		// This is how to subscribe("topic", queueSize, callbackFunction, hint)
+		// click_sub = n.subscribe("command_send", 10, &Route::_command_send, this);
 
 
 
 		// TODO: Make a fuction that listens for a joystick input
 
-		// This is how you make a string message
+		// This is how to make a string message
 		std_msgs::String msg;
 
      	std::stringstream ss;
      	ss << "Kitchen";
      	msg.data = ss.str();
 
-     	// This send a command string to next function.
-		_command_send_cb(msg);
+     	// This sends a command string to next function
+		_command_send(msg);
 	};
 };
 
 // This is where we start
 int main(int argc, char *argv[])
 {
-	ros::init(argc, argv, "go_to_point_smv");
+	ros::init(argc, argv, "go_to_point");
 
-	// Conctruct the class "Route"
-	Route r;
+	// Conctruct the class "GoToPoint"
+	GoToPoint g;
 
 	return 0;
 }
